4c2d555432e9be8febac0c83add32748cb6c5dd7
Ozgur Ersoz <zersz@paypal.com>
2024-02-08 14:44:29 +0100

    Update

--- Sources/include/PocketSVG.h
+++ Sources/include/PocketSVG.h
@@ -10,3 +10,32 @@
 #import "SVGImageView.h"
 #import "SVGLayer.h"
 #import "SVGEngine.h"
+
+
+// We need this check and declaration for Tuist because it does not generate SWIFTPM_MODULE_BUNDLE for objc SPM.
+#if SWIFT_PACKAGE && !(defined(SWIFTPM_MODULE_BUNDLE))
+
+NSBundle* SWIFTPM_MODULE_BUNDLE() {
+    NSString *bundleName = @"PocketSVG_PocketSVG";
+
+    
+    NSArray<NSURL*> *candidates = @[
+        NSBundle.mainBundle.resourceURL,
+        NSBundle.mainBundle.bundleURL
+    ];
+
+    for (NSURL* candiate in candidates) {
+        NSURL *bundlePath = [candiate URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.bundle", bundleName]];
+
+        NSBundle *bundle = [NSBundle bundleWithURL:bundlePath];
+        if (bundle != nil) {
+            return bundle;
+        }
+    }
+
+    @throw [[NSException alloc] initWithName:@"SwiftPMResourcesAccessor" reason:[NSString stringWithFormat:@"unable to find bundle named %@", bundleName] userInfo:nil];
+}
+
+#define SWIFTPM_MODULE_BUNDLE SWIFTPM_MODULE_BUNDLE()
+
+#endif
